38da9f723606a3e96590a949ed3112a6f4fa8aa4
shovan-642 <shovan642@gmail.com>
2025-03-28 12:44:31 +0600

    tutorial update and delete function done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -27,12 +27,12 @@ const client = new MongoClient(uri, {
       // Connect the client to the server	(optional starting in v4.7)
       await client.connect();
       // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
+      // await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
       const tutorialCollection = client.db('language-tutor').collection('tutor');
 
-      app.post("/tutor", async(req, res)=>{
+      app.post("/", async(req, res)=>{
         const newTutorial = req.body
         const result = await tutorialCollection.insertOne(newTutorial)
         res.send(result)
@@ -44,23 +44,66 @@ const client = new MongoClient(uri, {
         res.send(result)
       })
 
+      app.put('/tutor/:id', async(req, res)=>{
+        const id = req.params.id
+        const filter = {_id: new ObjectId(id)}
+        const options =  {upsert: true}
+        const updateTutorial = req.body
+        const updatedTutorial ={
+          $set:{
+             name : updateTutorial.name, 
+             tutor_email : updateTutorial.tutor_email,
+             image : updateTutorial.image,
+             language : updateTutorial.language,
+             price : updateTutorial.price,
+             description : updateTutorial.description, 
+             review : updateTutorial.review,
+             TutorImage : updateTutorial.TutorImage,
+            },
+            
+        }
+        const result = await tutorialCollection.updateOne(filter, updatedTutorial, options)
+        res.send(result) 
+      })
+
+      app.delete("/tutor/:id", async(req, res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await tutorialCollection.deleteOne(query)
+        res.send(result)
+      })
+
+      app.get("/tutor/:id", async(req,res)=>{
+        const id =  req.params.id
+        console.log(id)
+        const query = {_id: new ObjectId(id)}
+        const result = await tutorialCollection.findOne(query)
+        res.send(result)
+      })
+
       app.get("/myTutorials", async(req,res)=>{
-        const email =  req.query.email
+        const email =  req.query?.email
         const query = {tutor_email: email}
         const result = await tutorialCollection.find(query).toArray()
         res.send(result)
       })
 
+      app.get("/find-tutors/:category", async(req,res)=>{
+        const category = req.params.category
+        const query = {language: category}
+        const tutors = await tutorialCollection.find(query).toArray()
+        res.send(tutors)
+      })
+
 
     } finally {
       // Ensures that the client will close when you finish/error
-      // await client.close();
     }
   }
   run().catch(console.dir);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res)=>{
     res.send('server is running good')
 })
 
