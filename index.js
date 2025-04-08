5f30d87c898ee2c99c5d325427efe7c3cc2d48f1
shovan-642 <shovan642@gmail.com>
2025-04-08 22:19:56 +0600

    cors update

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const port = process.env.PORT || 3000;
 app.use(cors({
   origin: ["http://localhost:5173", "https://assignment-11-d8915.web.app/"],
   credentials: true,
+  methods: ["GET", "POST", "PATCH", "DELETE"], 
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(express.json())
