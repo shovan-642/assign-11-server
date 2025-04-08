997d7c88f8f65cbabeb25f26faa647c5c28c2876
shovan-642 <shovan642@gmail.com>
2025-04-08 22:13:32 +0600

    firebase hosting link add

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 
 
 app.use(cors({
-  origin: ["http://localhost:5173"],
+  origin: ["http://localhost:5173", "https://assignment-11-d8915.web.app/"],
   credentials: true,
 }))
 
